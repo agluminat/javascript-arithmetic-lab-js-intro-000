16c1b51b08592132ca442b399aa0d3abcc7e7474
Maxi Learner <sagmeister@gmail.com>
2019-01-22 01:35:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function dec(n) {
   return n -= 1
 }
 
-//function makeInt(n) {
-  //return parseInt("n", 10)
-//}
+function makeInt(n) {
+  return parseInt("n", 10)
+}
 
 function makeInt(n) {
   return parseInt("nonsense", 10)
