a95e76f0c8fb996d45d71e70c7d2fc8ab3d2341e
Maxi Learner <sagmeister@gmail.com>
2019-01-22 01:40:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,14 +27,7 @@ function makeInt(n) {
   return parseInt(n, 10)
 }
 
-function makeInt(n) {
-  return parseInt("nonsense", 10)
-}
 
 function preserveDecimal(n) {
   return parseFloat(n)
-}
-
-function preserveDecimal(n) {
-  return parseFloat("nonsense")
 }
